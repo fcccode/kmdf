b2cc984105d32e49e7c21283dbb4bee73c43b870
steward-fu <steward.fu@gmail.com>
2019-09-15 23:19:09 -0400

    init

--- asm/ioctl/sys/main.asm
+++ asm/ioctl/sys/main.asm
@@ -30,55 +30,48 @@ includelib C:\masm32\lib\wdf\kmdf\i386\1.9\wdfldr.lib
 includelib C:\masm32\lib\wdf\kmdf\i386\1.9\wdfdriverentry.lib
 
 public DriverEntry
-IOCTL_TEST  equ CTL_CODE(FILE_DEVICE_UNKNOWN, 800h, METHOD_BUFFERED, FILE_ANY_ACCESS)
+IOCTL_TEST equ CTL_CODE(FILE_DEVICE_UNKNOWN, 800h, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
 .const
-DEV_NAME word "\","D","e","v","i","c","e","\","f","i","r","s","t","I","O","C","T","L",0
-SYM_NAME word "\","D","o","s","D","e","v","i","c","e","s","\","f","i","r","s","t","I","O","C","T","L",0
-MSG db "KMDF driver tutorial for Device IOCTL",0
+DEV_NAME word "\","D","e","v","i","c","e","\","M","y","D","r","i","v","e","r",0
+SYM_NAME word "\","D","o","s","D","e","v","i","c","e","s","\","M","y","D","r","i","v","e","r",0
+MSG_IOCTL byte "IOCTL_TEST",0
 
 .code
-;//*** process CreateFile()
 IrpFileCreate proc Device:WDFDEVICE, Request:WDFREQUEST, FileObject:WDFFILEOBJECT
   invoke DbgPrint, $CTA0("IrpFieCreate")
   invoke WdfRequestComplete, Request, STATUS_SUCCESS
   ret
 IrpFileCreate endp
 
-;//*** process CloseHandle()
 IrpFileClose proc FileObject:WDFFILEOBJECT
   invoke DbgPrint, $CTA0("IrpFieClose")
   ret
 IrpFileClose endp
 
-;//*** process DeviceIoControl()
 IrpIOCTL proc Queue:WDFQUEUE, Request:WDFREQUEST, OutputBufferLength:DWORD, InputBufferLength:DWORD, IoControlCode:DWORD
   .if IoControlCode == IOCTL_TEST
-    invoke DbgPrint, $CTA0("_IOCTL_TEST")
+    invoke DbgPrint, offset MSG_IOCTL
   .endif
   invoke WdfRequestComplete, Request, STATUS_SUCCESS
   ret
 IrpIOCTL endp
 
-;//*** system will vist this routine when it needs to add new device
 AddDevice proc Driver:WDFDRIVER, pDeviceInit:PWDFDEVICE_INIT
   local device:WDFDEVICE
-  local file_cfg:WDF_FILEOBJECT_CONFIG
-  local ioqueue_cfg:WDF_IO_QUEUE_CONFIG
   local suDevName:UNICODE_STRING
   local szSymName:UNICODE_STRING
-  
-  invoke DbgPrint, offset MSG
+  local file_cfg:WDF_FILEOBJECT_CONFIG
+  local ioqueue_cfg:WDF_IO_QUEUE_CONFIG
+
   invoke RtlInitUnicodeString, addr suDevName, offset DEV_NAME
   invoke RtlInitUnicodeString, addr szSymName, offset SYM_NAME
   invoke WdfDeviceInitAssignName, pDeviceInit, addr suDevName
-  
   invoke WdfDeviceInitSetIoType, pDeviceInit, WdfDeviceIoBuffered
   invoke WDF_FILEOBJECT_CONFIG_INIT, addr file_cfg, offset IrpFileCreate, offset IrpFileClose, NULL
   invoke WdfDeviceInitSetFileObjectConfig, pDeviceInit, addr file_cfg, WDF_NO_OBJECT_ATTRIBUTES
   invoke WdfDeviceCreate, addr pDeviceInit, WDF_NO_OBJECT_ATTRIBUTES, addr device
   invoke WdfDeviceCreateSymbolicLink, device, addr szSymName
-  
   invoke WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE, addr ioqueue_cfg, WdfIoQueueDispatchSequential
   lea eax, ioqueue_cfg
   mov (WDF_IO_QUEUE_CONFIG ptr [eax]).EvtIoDeviceControl, offset IrpIOCTL
@@ -86,7 +79,6 @@ AddDevice proc Driver:WDFDRIVER, pDeviceInit:PWDFDEVICE_INIT
   ret
 AddDevice endp
 
-;//*** driver entry
 DriverEntry proc pOurDriver:PDRIVER_OBJECT, pOurRegistry:PUNICODE_STRING
   local config:WDF_DRIVER_CONFIG
   
