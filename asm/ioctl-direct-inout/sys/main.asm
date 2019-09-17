ebcc853991b3e9cc323a51df310cc9df5eca771a
steward-fu <steward.fu@gmail.com>
2019-09-16 23:55:26 -0400

    init

--- asm/ioctl-direct-inout/sys/main.asm
+++ asm/ioctl-direct-inout/sys/main.asm
@@ -30,45 +30,43 @@ includelib C:\masm32\lib\wdf\kmdf\i386\1.9\wdfldr.lib
 includelib C:\masm32\lib\wdf\kmdf\i386\1.9\wdfdriverentry.lib
 
 public DriverEntry
-IOCTL_DIRECT_GET  equ CTL_CODE(FILE_DEVICE_UNKNOWN, 800h, METHOD_OUT_DIRECT, FILE_ANY_ACCESS)
-IOCTL_DIRECT_SET  equ CTL_CODE(FILE_DEVICE_UNKNOWN, 801h, METHOD_OUT_DIRECT, FILE_ANY_ACCESS)
+IOCTL_GET equ CTL_CODE(FILE_DEVICE_UNKNOWN, 800h, METHOD_OUT_DIRECT, FILE_ANY_ACCESS)
+IOCTL_SET equ CTL_CODE(FILE_DEVICE_UNKNOWN, 801h, METHOD_IN_DIRECT, FILE_ANY_ACCESS)
 
 .const
-DEV_NAME word "\","D","e","v","i","c","e","\","f","i","r","s","t","I","O","C","T","L","-","D","i","r","e","c","t","-","O","u","t",0
-SYM_NAME word "\","D","o","s","D","e","v","i","c","e","s","\","f","i","r","s","t","I","O","C","T","L","-","D","i","r","e","c","t","-","O","u","t",0
-MSG db "KMDF driver tutorial for Device IOCTL-Direct-Out",0
+DEV_NAME word "\","D","e","v","i","c","e","\","M","y","D","r","i","v","e","r",0
+SYM_NAME word "\","D","o","s","D","e","v","i","c","e","s","\","M","y","D","r","i","v","e","r",0
+MSG_GET byte "IOCTL_GET",0
+MSG_SET byte "IOCTL_SET",0
 
 .data
-szBuffer byte 1024 dup(0)
+szBuffer byte 255 dup(0)
 
 .code
-;//*** process CreateFile()
 IrpFileCreate proc Device:WDFDEVICE, Request:WDFREQUEST, FileObject:WDFFILEOBJECT
   invoke DbgPrint, $CTA0("IrpFieCreate")
   invoke WdfRequestComplete, Request, STATUS_SUCCESS
   ret
 IrpFileCreate endp
 
-;//*** process CloseHandle()
 IrpFileClose proc FileObject:WDFFILEOBJECT
   invoke DbgPrint, $CTA0("IrpFieClose")
   ret
 IrpFileClose endp
 
-;//*** process DeviceIoControl()
 IrpIOCTL proc Queue:WDFQUEUE, Request:WDFREQUEST, OutputBufferLength:DWORD, InputBufferLength:DWORD, IoControlCode:DWORD
   local memory:WDFMEMORY
   local mdl:PMDL
   
   .if IoControlCode == IOCTL_DIRECT_SET
-    invoke DbgPrint, $CTA0("_IOCTL_DIRECT_SET")
+    invoke DbgPrint, offset MSG_SET
     invoke WdfRequestRetrieveInputWdmMdl, Request, addr mdl
 	  MmGetSystemAddressForMdlSafe mdl, LowPagePriority
 	  invoke memcpy, offset szBuffer, eax, InputBufferLength
-	  invoke DbgPrint, $CTA0("Buf: %s, Len:%d"), offset szBuffer, InputBufferLength
+	  invoke DbgPrint, $CTA0("Buffer: %s, Length:%d"), offset szBuffer, InputBufferLength
 	  invoke WdfRequestSetInformation, Request, InputBufferLength
   .elseif IoControlCode == IOCTL_DIRECT_GET
-    invoke DbgPrint, $CTA0("_IOCTL_DIRECT_GET")
+    invoke DbgPrint, offset MSG_GET
     invoke WdfRequestRetrieveOutputWdmMdl, Request, addr mdl
 	  MmGetSystemAddressForMdlSafe mdl, LowPagePriority
 	  invoke memcpy, eax, offset szBuffer, OutputBufferLength
@@ -78,25 +76,21 @@ IrpIOCTL proc Queue:WDFQUEUE, Request:WDFREQUEST, OutputBufferLength:DWORD, Inpu
   ret
 IrpIOCTL endp
 
-;//*** system will vist this routine when it needs to add new device
 AddDevice proc Driver:WDFDRIVER, pDeviceInit:PWDFDEVICE_INIT
   local device:WDFDEVICE
-  local file_cfg:WDF_FILEOBJECT_CONFIG
-  local ioqueue_cfg:WDF_IO_QUEUE_CONFIG
   local suDevName:UNICODE_STRING
   local szSymName:UNICODE_STRING
-  
-  invoke DbgPrint, offset MSG
+  local file_cfg:WDF_FILEOBJECT_CONFIG
+  local ioqueue_cfg:WDF_IO_QUEUE_CONFIG
+
   invoke RtlInitUnicodeString, addr suDevName, offset DEV_NAME
   invoke RtlInitUnicodeString, addr szSymName, offset SYM_NAME
   invoke WdfDeviceInitAssignName, pDeviceInit, addr suDevName
-  
   invoke WdfDeviceInitSetIoType, pDeviceInit, WdfDeviceIoBuffered
   invoke WDF_FILEOBJECT_CONFIG_INIT, addr file_cfg, offset IrpFileCreate, offset IrpFileClose, NULL
   invoke WdfDeviceInitSetFileObjectConfig, pDeviceInit, addr file_cfg, WDF_NO_OBJECT_ATTRIBUTES
   invoke WdfDeviceCreate, addr pDeviceInit, WDF_NO_OBJECT_ATTRIBUTES, addr device
   invoke WdfDeviceCreateSymbolicLink, device, addr szSymName
-  
   invoke WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE, addr ioqueue_cfg, WdfIoQueueDispatchSequential
   lea eax, ioqueue_cfg
   mov (WDF_IO_QUEUE_CONFIG ptr [eax]).EvtIoDeviceControl, offset IrpIOCTL
@@ -104,7 +98,6 @@ AddDevice proc Driver:WDFDRIVER, pDeviceInit:PWDFDEVICE_INIT
   ret
 AddDevice endp
 
-;//*** driver entry
 DriverEntry proc pOurDriver:PDRIVER_OBJECT, pOurRegistry:PUNICODE_STRING
   local config:WDF_DRIVER_CONFIG
   
--- asm/ioctl-direct-out/sys/build.bat
+++
@@ -1,5 +0,0 @@
-del /s /q main.sys
-c:\masm32\bin\ml /c /coff /Cp "main.asm"
-c:\masm32\bin\link /entry:FxDriverEntry@8 /MAP /IGNORE:4078 /nologo /driver /base:0x10000 /subsystem:native /align:64 /out:"main.sys" main.obj"
-del /s /q main.map
-del /s /q main.obj
