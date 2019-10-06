059785bb8cea2945b913ae8811dcfad532af7b91
steward-fu <steward.fu@gmail.com>
2019-10-06 02:56:41 -0400

    init

---
+++ pascal_pnp/hello/main.pas
@@ -0,0 +1,36 @@
+unit main;
+
+interface
+  uses
+    DDDK;
+    
+  const
+    DEV_NAME = '\Device\MyDriver';
+
+  function __DriverEntry(pOurDriver:PDRIVER_OBJECT; pOurRegistry:PUNICODE_STRING):NTSTATUS; stdcall;
+
+implementation
+
+function AddDevice(pOurDriver:WDFDRIVER; pDeviceInit:PWDFDEVICE_INIT):NTSTATUS; stdcall;
+var
+  device: WDFDEVICE;
+  suDevName: UNICODE_STRING;
+  pOurDevice: PDEVICE_OBJECT;
+  
+begin
+  DbgPrint('Hello, world!', []);
+  WdfDeviceInitAssignName(pDeviceInit, @suDevName);
+  WdfDeviceCreate(pDeviceInit, WDF_NO_OBJECT_ATTRIBUTES, @device);
+  Result:= STATUS_SUCCESS;
+end;
+
+function __DriverEntry(pOurDriver:PDRIVER_OBJECT; pOurRegistry:PUNICODE_STRING):NTSTATUS; stdcall;
+var
+  config: WDF_DRIVER_CONFIG;
+  
+begin
+  WDF_DRIVER_CONFIG_INIT(@config, AddDevice);
+  WdfDriverCreate(pOurDriver, pOurRegistry, WDF_NO_OBJECT_ATTRIBUTES, @config, WDF_NO_HANDLE);
+  Result:= STATUS_SUCCESS;
+end;
+end.
